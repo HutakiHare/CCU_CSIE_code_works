1aab1dea8caf6e421e39683833954e0259ecf9b1
HutakiHare <132063819+HutakiHare@users.noreply.github.com>
2024-04-22 10:35:12 +0800

    Add files via upload

---
+++ OOP_HW1_411410070.cpp
@@ -0,0 +1,36 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+class PQB{
+    public:
+
+    private:
+        int ID;
+        int nPQBID;
+    
+}
+
+class LQB{
+    public:
+
+    private:
+        int ID;
+        int PQBID;
+}
+
+class gate{
+    public:
+
+    private:
+        int precedence;
+        int LQBID;
+    
+}
+
+int main(){
+    int logQubits,gates,precedences,phyQubits,phyLinks;
+    vector<int> gate_id;
+
+    return 0;
+}
